1a5f86f6ef56962c17870caa366739487da6ab71
Frank Benady <knarfben110@gmail.com>
2020-06-11 20:45:18 +0200

    WIP

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './BurgerIngredient.css';
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ("bread-bottom"):
+      ingredient = <div className={classes.BreadBottom}></div>;
+      break;
+      
+    case ("bread-top"):
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
+        </div>);
+      break;
+    case ("meat"):
+      ingredient = <div className={classes.Meat}></div>;
+      break;
+    default:
+      break;
+  }
+  return <div>
+    Hello! component burgerIngredient
+  </div>;
+}
+
+export default burgerIngredient;
